040a1a735ef265bf680e55b11089edd650f1663e
TheoSherman29 <sherman.theodore@gmail.com>
2021-12-17 15:06:18 -0500

    Day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,74 +1,86 @@
-import sys
-import pathlib
-
-
 def intify(str):
     int_lst = []
     for i in str:
         int_lst.append(int(i))
     return int_lst
 
+def parse(file_name):
+    with open(file_name) as f:
+        data = f.read().split('\n\n')
+        str_nums, str_boards = data[0].split(','), data[1:]
 
-with open('example.txt') as f:
-    data = f.read().split('\n\n')
-    ex_nums, ex_boards = data[0].split(','), data[1:]
-    form_ex_nums = []
-    for num in ex_nums:
-        form_ex_nums.append(int(num))
-    form_ex_boards = []
-    for board in ex_boards:
+    draws = []
+    for num in str_nums:
+        draws.append(int(num))
+    board_rows = []
+    for board in str_boards:
         for row in board.splitlines():
             row = row.replace('  ', ' ')
-            form_ex_boards.append(intify(row.split()))
-    board_counter = 0
-    reform_ex_boards = []
-    print(form_ex_boards)
-    for row in range(len(form_ex_boards)):
-        reform_ex_boards.append([])
-        counter = 0
-        while counter < 5:
-            reform_ex_boards[board_counter].append(form_ex_boards[row])
-            counter += 1
-        board_counter += 1
+            board_rows.append(intify(row.split()))
 
-print(reform_ex_boards)
-form_ex_boards = dict(zip(range(len(reform_ex_boards)), reform_ex_boards))
-boards = form_ex_boards
+    boards = []
+    board = []
+    counter = 0
+    for row_i, row in enumerate(board_rows):
+        counter += 1
+        board.append(row)
+        if counter == 5:
+            counter = 0
+            boards.append(list(board))
+            board = []
 
-bingo_boards = {}
-for board in boards:
-    new_board = []
-    for row in boards[board]:
-        new_row = []
-        for num in row:
-            new_row.append([num, False])
-        new_board.append(new_row)
-    bingo_boards[board] = new_board
+    checks = [[[False for _ in range(5)] for _ in range(5)] for _ in boards]
+
+    return draws, boards, checks
 
 
-for i in range(len(form_ex_nums)):
-    for board in bingo_boards:
-        for row in bingo_boards[board]:
-            for num in row:
-                if form_ex_nums[i] == num[0]:
-                    num[1] = True
-    for board in bingo_boards:
-        for row in bingo_boards[board]:
-            winner = board
-            for i in range(5):
-                if row[i][1] == False:
-                    winner = None
-        for i in range(5):
-            winner = board
-            for j in range(5):
-                if bingo_boards[board][j][i][1] == False:
-                    winner = None
-            if winner:
-                print(bingo_boards[board])
-                break
-        if winner:
-            break
-    if winner:
-        break
+ex_draws, ex_boards, ex_checks = parse('example.txt')
+in_draws, in_boards, in_checks = parse('input.txt')
 
-#
+
+def is_winner(check):
+    columns = [True for _ in range(5)]
+    for r in range(5):
+        good_row = True
+        for d in range(5):
+            if not check[r][d]:
+                good_row = False
+                columns[d] = False
+        if good_row: return True
+    return any(columns)
+
+
+def game_p1(boards, checks, draws):
+    for draw in draws:
+        for board_i, board in enumerate(boards):
+            for row_i, row in enumerate(board):
+                for num_i, num in enumerate(row):
+                    if num == draw:
+                        checks[board_i][row_i][num_i] = True
+                        if is_winner(checks[board_i]):
+                            return get_score(board, checks[board_i], draw)
+
+
+def game_p2(boards, checks, draws):
+    wins = [False for _ in boards]
+    for draw in draws:
+        for board_i, board in enumerate(boards):
+            for row_i, row in enumerate(board):
+                for num_i, num in enumerate(row):
+                    if num == draw:
+                        checks[board_i][row_i][num_i] = True
+                        if is_winner(checks[board_i]):
+                            wins[board_i] = True
+            if all(wins): return get_score(board, checks[board_i], draw)
+
+
+def get_score(winner, check, draw):
+    sum = 0
+    for row in range(5):
+        for col in range(5):
+            if not check[row][col]: sum += winner[row][col]
+    return sum * int(draw)
+
+
+print(game_p1(in_boards, in_checks, in_draws))
+print(game_p2(in_boards, in_checks, in_draws))
